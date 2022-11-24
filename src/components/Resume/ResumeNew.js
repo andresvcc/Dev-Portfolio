de1aae0cab7b8ed9aa17b6a41837e51082f6b704
andresvcc <andresvcc88@gmail.com>
2022-11-24 21:36:30 +0100

    Add resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/andresvcc/portfolio/master/src/Assets/CV_Andres_Caballero.pdf";
+  "https://raw.githubusercontent.com/andresvcc/Dev-Portfolio/master/src/Assets/CV_Andres_Caballero.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
